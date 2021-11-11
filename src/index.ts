a87f32d8880eb0c35c491f2fdea7fb9e62068cc7
yujong-lee <yujonglee@g.skku.edu>
2021-11-12 02:21:11 +0900

    Fix import path

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,5 @@ import cssVariablesPolyfill from "css-vars-ponyfill";
 // CSS Variable support in legacy browsers
 cssVariablesPolyfill();
 
-
-export {default as ThemeProvider} from "./ThemeProvider.svelte";
-export * from './theme';
+export {default as ThemeProvider} from "../lib/ThemeProvider.svelte";
+export * from '../lib/theme';
